7c937892c193c827ed727e5769b042447b0c50f9
dkostiunin <melochevka@melochevka.ru>
2021-08-23 11:13:46 +0300

    1

--- say/say.js
+++ say/say.js
@@ -1,6 +1,7 @@
 import express from 'express';import {default as http_base} from 'http';import {default as io_base} from 'socket.io';import path from 'path';
-const app=express(),http=http_base.createServer(app),io=io_base(http),__dirname = path.resolve();
-app.use(express.static(".")); app.get('/', (req, res) => {res.sendFile (__dirname + '/voice/voice_recognition.html' )})
+const app=express(),http=http_base.createServer(app),io=io_base(http),__dirname = path.resolve(),PORT=process.env.PORT||80
+app.use(express.static(".")); 
+app.get('/', (req, res) => {res.sendFile (__dirname + '/voice/voice_recognition.html' )})
 
 io.on('connection', (socket) => {
 	io.to(socket.id).emit('chat',['подключился',socket.id]); console.log ('yes conns', socket.id);	
@@ -12,5 +13,5 @@ io.on('connection', (socket) => {
 	})	
 })
 
-http.listen(3000, () => {console.log('listening on *:3000')})
+http.listen(PORT, () => {console.log('listening on *:80')})
 
